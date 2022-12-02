ff714139fbbe781116cfa3d8526c1ad2d1a76109
anamk-168 <anamk168@gmail.com>
2022-12-02 19:56:54 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Mahasiswa.java
+++ src/praktek01/Mahasiswa.java
@@ -10,5 +10,14 @@ package praktek01;
  * @author anam.ict.wbm
  */
 public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
+    
+    void cetakinfo(){
+        System.out.println("Nama  : " +nama);
+        System.out.println("NPM  : " +NPM);
+        System.out.println("IPK  : " +IPK);
+    }
     
 }
