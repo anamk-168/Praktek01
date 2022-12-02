4e7bb52ff9e4044db5d56d0572ebb202c706d017
anamk-168 <anamk168@gmail.com>
2022-12-02 20:06:59 +0800

    Memanggil Method

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -12,8 +12,12 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
       Mahasiswa Harry = new Mahasiswa();
+      
       Harry.nama="Harry Potter";
       Harry.NPM="19639999";
       Harry.IPK=3.4;
+      
+      Harry.cetakinfo();
+      
     }
 }
